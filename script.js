9cc32295f02fb5933c97bd8be2b4fac03719d9a0
CatMemeindonesia <susantochen716@gmail.com>
2025-10-25 14:00:28 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+const search = document.getElementById("Search");
+const listItems = document.getElementById("ListItems");
+
+let dataAvaible = ["minecraft","roblox","roadmap","kemenku","gameBaru!"];
+
+search.addEventListener('input', function(){
+    let valueSearch = search.value.replace(/\s+/g, "").toLocaleLowerCase();
+    let dataFind = dataAvaible.find(datas => datas.toLocaleLowerCase().trim().includes(valueSearch))
+
+    if(dataFind) {
+        if(valueSearch.trim() === "") {
+            listItems.innerHTML = "";
+            return
+        }
+        let li = document.createElement("li");
+        listItems.innerHTML = "";
+        li.textContent = dataFind;
+        listItems.appendChild(li);
+    } else {
+        listItems.innerHTML = "";
+    };
+});
